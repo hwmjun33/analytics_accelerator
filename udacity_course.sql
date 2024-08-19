2fb6c612c473b18d534edf59e27ff49ac015476f
hwmjun33 <hwm.jun33@gmail.com>
2024-08-19 17:04:27 +0800

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -1,2 +1,7 @@
+-- Showing all columns
 SELECT *
-  FROM demo.orders
+  FROM orders
+
+-- Showing only the id, account_id, and occurred_at columns for all orders in the orders table
+SELECT id, account_id, occurred_at
+  FROM orders
