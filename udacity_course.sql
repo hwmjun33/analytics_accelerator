88913b2c6295e1aa098fdbd1d03b25c9cd1ada32
hwmjun33 <hwm.jun33@gmail.com>
2024-08-19 17:22:47 +0800

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -5,3 +5,12 @@ SELECT *
 -- Showing only the id, account_id, and occurred_at columns for all orders in the orders table
 SELECT id, account_id, occurred_at
   FROM orders
+
+### Limits
+/*
+Limiting the response to only the first 15 rows and includes the date, account_id, and channel fields in the web_event table. 
+Note: usually LIMIT is put at the end of the query
+*/
+SELECT occurred_at, account_id, channel
+FROM web_events
+LIMIT 15;
