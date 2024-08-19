318663d1f4f2ca57875d20b562a9cbc13cc8bb9e
hwmjun33 <hwm.jun33@gmail.com>
2024-08-20 07:40:31 +0800

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -6,7 +6,7 @@ SELECT *
 SELECT id, account_id, occurred_at
   FROM orders
 
-### Limits
+### LIMIT
 /*
 Limiting the response to only the first 15 rows and includes the date, account_id, and channel fields in the web_event table. 
 Note: usually LIMIT is put at the end of the query
@@ -14,3 +14,39 @@ Note: usually LIMIT is put at the end of the query
 SELECT occurred_at, account_id, channel
 FROM web_events
 LIMIT 15;
+
+### ORDER BY
+/*
+Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
+*/
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at
+LIMIT 10;
+
+/*
+Write a query to return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd.
+*/
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC 
+LIMIT 5;
+
+/*
+Write a query to return the lowest 20 orders in terms of smallest total_amt_usd. Include the id, account_id, and total_amt_usd.
+*/
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd
+LIMIT 20;
+
+
+
+
+
+
+
+
+
+
+
