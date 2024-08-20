18f160b658043768b03c4355f3f989f4cebcb8e5
hwmjun33 <hwm.jun33@gmail.com>
2024-08-20 13:23:07 +0800

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -101,7 +101,7 @@ FROM accounts
 WHERE name = 'Exxon Mobil';
 
 
-### Arithmetic Operations
+### Arithmetic Operations (+,-,*,/)
 
 Derived Columns
 -- Creating a new column that is a combination of existing columns is known as a Derived Column (or "calculated" or "computed" column). 
@@ -143,8 +143,37 @@ FROM orders
 LIMIT 10;
 
 
+### LOGICAL OPERATORS (LIKE, IN, NOT, AND&BETWEEN, OR)
+-- LIKE This allows you to perform operations similar to using WHERE and =, but for cases when you might not know exactly what you are looking for.
+-- IN This allows you to perform operations similar to using WHERE and =, but for more than one condition.
+-- NOT This is used with IN and LIKE to select all of the rows NOT LIKE or NOT IN a certain condition.
+-- AND & BETWEEN These allow you to combine operations where all combined conditions must be true.
+-- OR This allows you to combine operations where at least one of the combined conditions must be true.
 
+### LIKE
+-- The LIKE operator is extremely useful for working with text. You will use LIKE within a WHERE clause. The LIKE operator is frequently used with %. 
+-- The % tells us that we might want any number of characters leading up to a particular set of characters or following a certain set of characters, 
+-- as we saw with the google syntax above. Remember you will need to use single quotes for the text you pass to the LIKE operator, 
+-- because of this lower and uppercase letters are not the same within the string. Searching for 'T' is not the same as searching for 't'. 
 
+/*
+Use the accounts table to find:
+- All the companies whose names start with 'C'.
+- All companies whose names contain the string 'one' somewhere in the name.
+- All companies whose names end with 's'.
+*/
+
+SELECT name
+FROM accounts
+WHERE name LIKE 'C%';
+
+SELECT name
+FROM accounts
+WHERE name LIKE '%one%';
+
+SELECT name
+FROM accounts
+WHERE name LIKE '%s';
 
 
 
