c88533fdcd0e0e3122ab081106cf49b2e4a7e0e8
hwmjun33 <hwm.jun33@gmail.com>
2024-09-04 16:08:12 +0800

    Update udacity_course3.sql

--- udacity_course3.sql
+++ udacity_course3.sql
@@ -1,1 +1,9 @@
+### NULLS
+-- Defintion: NULLS are not zero; they are cells where data does not exist.
+-- When identifying NULLS in a WHERE clause, we write IS NULL or IS NOT NULL. (We don't use = because NULL is not a value in SQL)
+-- NULLS often occur when performing a LEFT OR RIGHT JOIN as we saw in previous lessons when certain rows in the left table of a left join 
+-- are not matched with rows in the right table; NULLS can also occur from simply missing data in our database.
+
+
+
 
