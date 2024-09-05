e8b7d83dd274bcfc6b3a703cfb6904f93f59b0fd
hwmjun33 <hwm.jun33@gmail.com>
2024-09-05 09:20:31 +0800

    Update udacity_course3.sql

--- udacity_course3.sql
+++ udacity_course3.sql
@@ -4,6 +4,77 @@
 -- NULLS often occur when performing a LEFT OR RIGHT JOIN as we saw in previous lessons when certain rows in the left table of a left join 
 -- are not matched with rows in the right table; NULLS can also occur from simply missing data in our database.
 
+e.g. choose a column to drop into the aggregation function of COUNT
+SELECT COUNT(accounts.id)
+FROM accounts;
 
+OUTPUT: 351
+
+/* The Count function is just looking at NON-NULL data, meaning it does not count rows that have NULL values. */
+
+e.g.(2)
+SELECT COUNT(primary_poc) AS account_primary_poc_count
+FROM demo.accounts
+
+OUTPUT: 345 
+-- Note: There are 9 rows with null data so 351 - 9 = 345.
+
+  
+### SUM
+-- Unlike COUNT, SUM can only be used on numeric columns. But SUM will ignore NULL values, treating them as 0. 
+
+/*
+Aggregation Questions
+Use the SQL environment below to find the solution for each of the following questions. If you get stuck or want to check your answers, you can find the answers at the top of the next concept.
+1. Find the total amount of poster_qty paper ordered in the orders table.
+2. Find the total amount of standard_qty paper ordered in the orders table.
+3. Find the total dollar amount of sales using the total_amt_usd in the orders table.
+4. Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table. This should give a dollar amount for each order in the table.
+5. Find the standard_amt_usd per unit of standard_qty paper. Your solution should use both an aggregation and a mathematical operator.
+*/
+
+SOLUTIONS
+1. 
+SELECT SUM(poster_qty) AS total_poster_sales
+FROM orders;
+/* OUTPUT:
+sum
+723646
+*/
+
+2. 
+SELECT SUM(standard_qty) AS total_standard_sales
+FROM orders;
+/* OUTPUT:
+sum
+1938346
+*/
+
+3.
+SELECT SUM(total_amt_usd) AS total_dollar_sales
+FROM orders;
+/* OUTPUT:
+sum
+23141511.83
+*/
+
+4.
+SELECT standard_amt_usd standard, gloss_amt_usd gloss, (standard_amt_usd + gloss_amt_usd) total_standard_gloss
+FROM orders;
+/* OUTPUT:
+standard	gloss	total
+613.77	164.78	778.55
+...
+*/
+
+5.
+SELECT SUM(standard_amt_usd) AS total_amount,
+SUM(standard_qty) AS total_quantity,
+SUM(standard_amt_usd) / SUM(standard_qty) AS standard_price_per_unit
+FROM orders;
+/* OUTPUT:
+total_amount	total_quantity	per_unit
+9672346.54	  1938346	        4.9900000000000000
+*/
 
 
